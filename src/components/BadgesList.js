312fdbca4458de5d1e7622ae639ed31dd8cb12c2
María Pérez <maria_erez@hotmail.es>
2020-08-13 21:31:05 -0500

    18. editing data

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Gravatar from'./Gravatar'
-
 
 import './styles/BadgesList.css';
+import Gravatar from './Gravatar';
 
 class BadgesListItem extends React.Component {
   render() {
@@ -12,7 +11,6 @@ class BadgesListItem extends React.Component {
         <Gravatar
           className="BadgesListItem__avatar"
           email={this.props.badge.email}
-          alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
         />
 
         <div>
@@ -47,7 +45,12 @@ class BadgesList extends React.Component {
           {this.props.badges.map(badge => {
             return (
               <li key={badge.id}>
-                <BadgesListItem badge={badge} />
+                <Link
+                  className="text-reset text-decoration-none"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  <BadgesListItem badge={badge} />
+                </Link>
               </li>
             );
           })}
